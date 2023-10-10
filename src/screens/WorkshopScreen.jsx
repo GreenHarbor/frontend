04b6fb911392ddf6e67aa4175447ff9cadee3d38
franky-lim24 <franky.2021@scis.smu.edu.sg>
2023-10-10 15:17:36 +0800

    Update WorkshopScreen.jsx

--- src/screens/WorkshopScreen.jsx
+++ src/screens/WorkshopScreen.jsx
@@ -1,8 +1,64 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, Text, View } from 'react-native';
+import Header from '../shared/Header';
+import Title from '../shared/Title';
+
+const data = [
+  {
+    name: 'Terrarium',
+    location: '81 Victoria St, Singapore 188065',
+    date: 'Sep 10th, 2023',
+    time: '10:00 pm',
+    image: 'https://i.imgur.com/7zK4FgX.png',
+  },
+  {
+    name: 'Terrarium',
+    location: '81 Victoria St, Singapore 188065',
+    date: 'Sep 10th, 2023',
+    time: '10:00 pm',
+    image: 'https://i.imgur.com/7zK4FgX.png',
+  },
+  {
+    name: 'Terrarium',
+    location: '81 Victoria St, Singapore 188065',
+    date: 'Sep 10th, 2023',
+    time: '10:00 pm',
+    image: 'https://i.imgur.com/7zK4FgX.png',
+  },
+];
 
 const WorkshopScreen = () => {
-  return <View className="w-screen h-screen"></View>;
+  const [current, setCurrent] = useState(0);
+
+  const renderWorkshops = () => {
+    return (
+      <View className="w-full h-full">
+        <Image source={data[current % 3].image} />
+        <Image source={data[(current + 1) % 3].image} />
+        <Image source={data[(current + 2) % 3].image} />
+      </View>
+    );
+  };
+
+  return (
+    <View className="w-screen h-screen bg-white">
+      <Header />
+      <View className="w-screen ml-4">
+        <Title title="Workshop" />
+        <View className="w-full flex flex-row mt-4">
+          <View>
+            <Text className="text-3xl font-[Orkney]">{data[current].name}</Text>
+            <Text className="font-[Orkney]">{data[current].location}</Text>
+          </View>
+          <View className="ml-auto mr-8 items-end">
+            <Text className="font-[Orkney]">{data[current].date}</Text>
+            <Text className="font-[Orkney]">{data[current].time}</Text>
+          </View>
+        </View>
+        {renderWorkshops()}
+      </View>
+    </View>
+  );
 };
 
 export default WorkshopScreen;
