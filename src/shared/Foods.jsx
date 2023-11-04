6b676625d8f654c0134453ffba094a1bb7de8877
franky-lim24 <franky.2021@scis.smu.edu.sg>
2023-11-04 23:45:26 +0800

    add swiper card

--- src/shared/Foods.jsx
+++ src/shared/Foods.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Text, View, ScrollView, Image } from 'react-native';
+import { Text, View, ScrollView, Image, Button } from 'react-native';
 import Title from './Title';
 import Clock from '../../assets/clock.png';
 import Map from '../../assets/location.png';
+import Swiper from 'react-native-deck-swiper';
 
 const data = [
   {
@@ -15,29 +16,29 @@ const data = [
     location: '80 Stamford Rd, Singapore 178902',
   },
   {
-    name: 'Chicken Satay',
+    name: 'Grilled Chicken',
     description:
       'Skewered, grilled chicken marinated in aromatic spices, served with peanut sauce, a flavorful appetizer or main dish',
     image:
-      'https://carlsbadcravings.com/wp-content/uploads/2019/04/Chicken-Satay-8.jpg',
+      'https://fitfoodiefinds.com/wp-content/uploads/2021/05/chicken-marinade-7-sq.jpg',
     timePosted: '30 mins ago',
     location: '80 Stamford Rd, Singapore 178902',
   },
   {
-    name: 'Chicken Satay',
+    name: 'Salad',
     description:
       'Skewered, grilled chicken marinated in aromatic spices, served with peanut sauce, a flavorful appetizer or main dish',
     image:
-      'https://carlsbadcravings.com/wp-content/uploads/2019/04/Chicken-Satay-8.jpg',
+      'https://www.eatingwell.com/thmb/rmLlvSjdnJCCy_7iqqj3x7XS72c=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/chopped-power-salad-with-chicken-0ad93f1931524a679c0f8854d74e6e57.jpg',
     timePosted: '30 mins ago',
     location: '80 Stamford Rd, Singapore 178902',
   },
   {
-    name: 'Chicken Satay',
+    name: 'Pizza',
     description:
       'Skewered, grilled chicken marinated in aromatic spices, served with peanut sauce, a flavorful appetizer or main dish',
     image:
-      'https://carlsbadcravings.com/wp-content/uploads/2019/04/Chicken-Satay-8.jpg',
+      'https://tastesbetterfromscratch.com/wp-content/uploads/2023/06/Pepperoni-Pizza-1.jpg',
     timePosted: '30 mins ago',
     location: '80 Stamford Rd, Singapore 178902',
   },
@@ -45,41 +46,56 @@ const data = [
 
 const Foods = () => {
   return (
-    <View className="w-[80%] h-full flex flex-row">
-      {data.map((food, index) => {
-        return (
-          <View
-            key={index}
-            className="w-[60%] h-full bg-secondary-color rounded-xl ml-12"
-          >
-            <Image
-              source={{ uri: food.image }}
-              alt="food"
-              className="w-32 h-32 rounded-full absolute -top-8 -left-8 "
-            />
-            <View className="w-[80%] mt-28 mx-auto flex flex-col justify-evenly h-[70%]">
-              <Title title={food.name} size="small" />
-              <Text className="text-black text-sm font-['Orkney'] mt-4">
-                {food.description}
-              </Text>
-              <View className="flex flex-col mt-auto mb-4 w-[80%]">
-                <View className="flex flex-row items-center gap-4 mt-auto">
-                  <Image source={Clock} alt="clock" />
-                  <Text className="text-black text-xs font-['Orkney']">
-                    {food.timePosted}
-                  </Text>
-                </View>
-                <View className="flex flex-row items-center gap-4">
-                  <Image source={Map} alt="map" />
-                  <Text className="text-black text-xs font-['Orkney']">
-                    {food.location}
-                  </Text>
+    <View className="w-[80%] h-full mb-auto">
+      <Swiper
+        cards={data}
+        renderCard={(food, index) => {
+          return (
+            <View
+              key={index}
+              className="w-[60%] bg-secondary-color rounded-xl ml-12 h-[60%] pb-4"
+            >
+              <Image
+                source={{ uri: food.image }}
+                alt="food"
+                className="w-32 h-32 rounded-full absolute -top-8 -left-8 "
+              />
+              <View className="w-[80%] mt-28 mx-auto flex flex-col justify-evenly h-[70%]">
+                <Title title={food.name} size="small" />
+                <Text className="text-black text-sm font-['Orkney'] mt-4">
+                  {food.description}
+                </Text>
+                <View className="flex flex-col mt-auto mb-4 w-[80%]">
+                  <View className="flex flex-row items-center gap-4 mt-auto">
+                    <Image source={Clock} alt="clock" />
+                    <Text className="text-black text-xs font-['Orkney']">
+                      {food.timePosted}
+                    </Text>
+                  </View>
+                  <View className="flex flex-row items-center gap-4">
+                    <Image source={Map} alt="map" />
+                    <Text className="text-black text-xs font-['Orkney']">
+                      {food.location}
+                    </Text>
+                  </View>
                 </View>
               </View>
             </View>
-          </View>
-        );
-      })}
+          );
+        }}
+        onSwiped={(cardIndex) => {
+          console.log(cardIndex);
+        }}
+        onSwipedAll={() => {
+          console.log('onSwipedAll');
+        }}
+        cardIndex={0}
+        stackSize={2}
+        backgroundColor={'transparent'}
+        infinite={true}
+        verticalSwipe={false}
+        showSecondCard={true}
+      ></Swiper>
     </View>
   );
 };
